7b9f9279fa1ea03f2e2f9429da8d72758d03c65d
Jerry Guo <jerry.guo@access-company.com>
2019-12-27 13:53:03 +0800

    add kitty supervisor to otp application

--- elixir_workshop/otp/lib/otp_application.ex
+++ elixir_workshop/otp/lib/otp_application.ex
@@ -3,7 +3,8 @@ defmodule OtpApplication do
 
   def start(_type, _args) do
     children = [
-      {StackSupervisor, strategy: :one_for_one, name: StackSupervisor}
+      {StackSupervisor, strategy: :one_for_one, name: StackSupervisor},
+      {KittySupervisor, strategy: :one_for_one, name: KittySupervisor},
     ]
 
     Supervisor.start_link(children, strategy: :rest_for_one, name: __MODULE__)
